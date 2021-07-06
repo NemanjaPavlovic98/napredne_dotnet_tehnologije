85fd1aa51502feffd5e51e143f72118e943a7b57
NemanjaPavlovic <pavlovic.nemanja.1998@gmail.com>
2021-07-06 18:17:46 +0200

    all

---
+++ backend/MoviesAPI/DTOs/ActorCreationDTO.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.DTOs
+{
+    public class ActorCreationDTO
+    {
+        [Required]
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Biography { get; set; }
+        public IFormFile Picture { get; set; }
+    }
+}
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiURL: 'https://169.254.182.36:5101/api'
+};
